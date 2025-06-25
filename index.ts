5204167bc0bcca971ff41799d99ebfdd9cd75bb8
PiQuark6046 <j.w.so@adguard.com>
2025-06-25 22:31:54 +0000

    fix: process.argv issue

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { RunPrompts } from './prompts.js'
 import { RunCommander } from './commander.js'
 import * as Process from 'node:process'
 
-if (Process.argv.length === 0) {
+if (Process.argv.filter(Arg => !Arg.includes('node') && !Arg.includes('parkedDomain2')).length === 0) {
   await RunPrompts()
 } else {
   await RunCommander()
