1c9e58db7400a3aed87f1036cb46b138e0f10cf9
PiQuark6046 <j.w.so@adguard.com>
2025-06-25 20:45:41 +0000

    fix: missing commander.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { RunPrompts } from './prompts.js'
 import { RunCommander } from './commander.js'
 import * as Process from 'node:process'
 
-if (process.argv.length === 0) {
+if (Process.argv.length === 0) {
   await RunPrompts()
 } else {
   await RunCommander()
